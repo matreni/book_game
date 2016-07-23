4085b0088a0a3882688abfdbf8c13f43a88c9839
dzekegor <gubanov.egor2010@yandex.ru>
2016-07-23 13:45:38 +0400

    Library и  book_12

---
+++ Library.py
@@ -0,0 +1,19 @@
+from book_1 import d as d1
+from book_2 import d as d2
+from book_4 import d as d4
+from book_6 import d as d6
+from book_8 import d as d8
+from book_10 import d as d10
+from book_11 import d as d11
+from book_12 import d as d12
+from book_13 import d as d13
+d = {}
+d.update(d1)
+d.update(d2)
+d.update(d4)
+d.update(d6)
+d.update(d8)
+d.update(d10)
+d.update(d11)
+d.update(d12)
+d.update(d13)
